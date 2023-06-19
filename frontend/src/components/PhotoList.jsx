c2bdd351cf54538559ed83d3cc47022702f46e04
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-18 23:56:11 -0700

    cleaning code

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,7 +3,7 @@ import '../styles/PhotoList.scss';
 import PhotoListItem from './PhotoListItem';
 import photos from '../mocks/photos';
 
-const PhotoList = ({ photos, handleFavoriteClick, isFavorite }) => {
+const PhotoList = ({ handleFavoriteClick, isFavorite }) => {
   return (
     <ul className="photo-list">
       {photos.map((photo) => (
