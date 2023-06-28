95c1318084343c46018db74dc7bbebebe0bd9b66
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-27 19:43:18 -0700

    changed how PhotoDetailsModal renders

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,30 +1,60 @@
-import React from 'react';
-import '../styles/PhotoDetailsModal.scss';
-import PhotoList from '../components/PhotoList';
+import React from "react";
+import "../styles/PhotoDetailsModal.scss";
+import PhotoList from "../components/PhotoList";
+import PhotoListItem from "../components/PhotoListItem";
 
-export const PhotoDetailsModal = ({ onClose, photo, handleFavoriteClick, favorites }) => {
+export const PhotoDetailsModal = ({
+  onClose,
+  photo,
+  handleFavoriteClick,
+  favorites,
+}) => {
   return (
-    <div className='photo-details-modal'>
-      <button className='photo-details-modal--close-button' onClick={onClose}>
-        <svg width="24" height="24" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div className="photo-details-modal">
+      <button className="photo-details-modal--close-button" onClick={onClose}>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g clipPath="url(#clip0_428_287)">
-            <path d="M14.0625 3.9375L3.9375 14.0625" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M14.0625 14.0625L3.9375 3.9375" stroke="black" strokeLinecap="round" strokeLinejoin="round"/>
+            <path
+              d="M14.0625 3.9375L3.9375 14.0625"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M14.0625 14.0625L3.9375 3.9375"
+              stroke="black"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
           </g>
           <defs>
             <clipPath id="clip0_428_287">
-              <rect width="18" height="18" fill="white"/>
+              <rect width="18" height="18" fill="white" />
             </clipPath>
           </defs>
         </svg>
       </button>
       <div className="photo-details-modal--content">
-        <img src={photo.urls.regular} className="photo-details-modal--image" />
+        <PhotoListItem
+          name={photo.user.name}
+          location={photo.location}
+          profile={photo.user.profile}
+          handleFavoriteClick={() => handleFavoriteClick(photo.id)}
+          handlePhotoClick={() => handlePhotoClick(photo.id)}
+          isFavorite={favorites.includes(photo.id)}
+          photoURL={photo.urls.regular}
+        />
         <div className="photo-details-modal--header">
           <h2>Similar Photos</h2>
         </div>
-        <div className='.photo-details-modal--images'>
-          <PhotoList 
+        <div className=".photo-details-modal--images">
+          <PhotoList
             favorites={favorites}
             photos={photo.similar_photos}
             handleFavoriteClick={handleFavoriteClick}
@@ -36,4 +66,3 @@ export const PhotoDetailsModal = ({ onClose, photo, handleFavoriteClick, favorit
 };
 
 export default PhotoDetailsModal;
-
